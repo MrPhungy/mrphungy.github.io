605513f7563b44d2eb68e38e736e5e7e0c11ffa6
Kevin Phung <kevinphung216@yahoo.com>
2025-01-31 00:28:37 -0600

    added MorseCodeTranslator proj

---
+++ html/MyProjects/MorseCodeTranslator/script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    const outputElement = document.getElementById('output');
+    outputElement.innerText = 'Translation'; // Default message
+    outputElement.classList.add('default-message'); // Apply the default-message class
+};
